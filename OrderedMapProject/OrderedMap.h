6da5b3ae6f4b1c642b214cde4ef22cbcf7d91da8
Kevin Long <kevinlongathome@outlook.com>
2019-06-10 18:56:54 -0700

    Second Commit

--- OrderedMapProject/OrderedMap.h
+++ OrderedMapProject/OrderedMap.h
@@ -1,1 +1,28 @@
-#pragma once
+#ifndef _ORDERED_MAP_
+#define _ORDERED_MAP_
+#include <list>
+#include <vector>
+
+template <class K, class V>
+class OrderedMap
+{
+private:
+	std::list<K> keys;
+public:
+	void clear() {}
+	bool containsKey(K key) {}
+	bool containsValue(V value) {}
+	V get(K key) {}
+	std::vector getKeys()
+	{
+		if (keys.empty())
+		{
+			return
+		}
+	}
+	bool isEmpty() {}
+	void put(K key, V value) {}
+	V remove(K key) {}
+};
+
+#endif
