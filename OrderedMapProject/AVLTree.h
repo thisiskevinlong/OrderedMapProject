e4bc7bd3a462115d96b11d8d4f35755f75c3e0a4
nerdromere <nerdromere@gmail.com>
2019-06-12 17:28:49 -0700

    Adding the Node.h File

--- OrderedMapProject/AVLTree.h
+++ OrderedMapProject/AVLTree.h
@@ -1,1 +1,21 @@
-#pragma once
+#ifndef AVLTREE_H_INCLUDED
+#define AVLTREE_H_INCLUDED
+
+typedef struct node
+{
+	int data;
+	struct node* left;
+	struct node* right;
+	int      height;
+} node;
+
+
+void dispose(node* t);
+node* find(int e, node* t);
+node* find_min(node* t);
+node* find_max(node* t);
+node* insert(int data, node* t);
+node* delete(int data, node* t);
+void display_avl(node* t);
+int get(node* n);
+#endif // AVLTREE_H_INCLUDED
--- OrderedMapProject/HashMap.h
+++ OrderedMapProject/HashMap.h
@@ -9,7 +9,14 @@ private:
 	AVLTree** trees;
 	int capacity;
 	int count;
-
+public:
+	HashMap* initializeHashMap();
+	void deleteHashMap();
+	bool contains(K);
+	void insert(HashMap* map, char key[], double value);
+	double get(HashMap* map, char key[]);
+	bool delete(HashMap* map, char key[]);
 };
+
 
 #endif // !
